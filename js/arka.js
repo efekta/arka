f3fc74f2cea5b0a768f36932df721b95d7dd327b
Ptashkina Liliya <efekta9@gmail.com>
2019-03-29 13:30:33 +0300

    slick slider

--- js/arka.js
+++ js/arka.js
@@ -175,12 +175,50 @@ $('.gallery .group-item').each(function(index){
     obj.find('.gallery-item:lt('+x+')').show();
   }); 
 
-  $('.review .review-wrapper').owlCarousel({
-    loop:false,
-    margin:0,
-    nav:false,
-    items: 3
-  })
+  // $('.review .review-wrapper').owlCarousel({
+  //   loop:false,
+  //   margin:0,
+  //   nav:false,
+  //   items: 3,
+  //   responsiveClass:true
+  //   responsive:{
+  //       0:{
+  //           items:1,
+  //           nav:true
+  //       },
+  //       600:{
+  //           items:1,
+  //           nav:false
+  //       },
+  //       1000:{
+  //           items:2,
+  //           nav:true,
+  //           loop:false
+  //       }
+  //   }
+  // })
+//   $('.review .review-wrapper').owlCarousel({
+//     loop:true,
+//     margin:0,
+//     nav:false,
+//     items: 3,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:2,
+//             nav:false
+//         },
+//         1000:{
+//             items:3,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
 });
 
 $(document).ready(overallHeight);
@@ -242,9 +280,60 @@ $(function() {
   var accordion = new Accordion($('#accordion, #accordion2'), false);
 });
 
+$('.review .review-wrapper').slick({
+  dots: false,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:1,
+//             nav:false
+//         },
+//         1000:{
+//             items:2,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
 }
-
 
 
 
